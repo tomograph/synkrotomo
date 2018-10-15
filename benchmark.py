de922b61844c8df4cd4fda3359b9fed05336561e
Unknown <memo@di.ku.dk>
2018-10-15 13:06:24 +0200

    Added futhark fp test

--- benchmark.py
+++ benchmark.py
@@ -3,6 +3,8 @@ import numpy as np
 import pylab
 import time
 import sys
+from spMVmult import spMVmult
+from scipy.sparse import csr_matrix
 
 
 def test_astra_algorithm(name, phantom, proj_geom, vol_geom, iterations):
@@ -73,32 +75,66 @@ def test_astra_FP(phantom, proj_geom, vol_geom):
 
 def hollow_cube_phantom(size):
     # Create a simple hollow cube phantom
+    if size < 16:
+        raise "This phantom is too small"
+
+    if size < 100:
+        cube = np.zeros((size,size,size))
+        cube[2:size-3,2:size-3,2:size-3] = 1
+        cube[5:size-8,5:size-8,5:size-8] = 0
+        return cube
+
     cube = np.zeros((size,size,size))
     cube[17:size-15,17:size-15,17:size-15] = 1
     cube[33:size-31,33:size-31,33:size-31] = 0
     return cube
 
+def test_futhark_fp(phantom, proj_geom, vol_geom):
+    # Create projection data
+    proj_id, proj_data = astra.create_sino3d_gpu(phantom, proj_geom, vol_geom)
+
+    #convert system matrix to scipy sparse matrix
+    A = csr_matrix(proj_data)
+    #convert to correct format for futhark sparse matrix vector mult
+    column_indexes = A.indices
+    data = A.data
+    shape = A.getnnz(1)
+    size = phantom.shape[1]
+    vector = phantom.reshape((size*size*size, 1))
+
+    spMVmult = spMVmult()
+    start = time.time()
+    res = spMVmult.main(column_indexes, data, shape, vector)
+    end = time.time()
+
+    elapsed = (end - start)*1000
+    print("The futhark fp: took %d milliseconds" % (elapsed))
+
+    return res.reshape((size,size,size)), elapsed
 
 def main(argv):
-    size = 256
-    iterations = 200
-
+    size = 16
     vol_geom = astra.create_vol_geom(size, size, size)
 
-    angles = np.linspace(0, np.pi, 180,False)
+    angles = np.linspace(0, np.pi, 20,False)
     proj_geom = astra.create_proj_geom('parallel3d', 1.0, 1.0, size, size, angles)
     cube = hollow_cube_phantom(size)
 
-    reconstruction, elapsed = test_astra_algorithm('SIRT3D_CUDA', cube, proj_geom, vol_geom, iterations)
-    bp, elapsed = test_astra_algorithm('BP3D_CUDA', cube, proj_geom, vol_geom, 1)
-    fp, elapsed = test_astra_FP(cube, proj_geom, vol_geom)
+    #Test astra functions
+    reconstruction_astra, elapsed = test_astra_algorithm('SIRT3D_CUDA', cube, proj_geom, vol_geom, iterations)
+    bp_astra, elapsed = test_astra_algorithm('BP3D_CUDA', cube, proj_geom, vol_geom, 1)
+    fp_astra, elapsed = test_astra_FP(cube, proj_geom, vol_geom)
 
     #Save slice of the recosntruction
-    pylab.imsave("reconstruction.png",reconstruction[:,:,size/2])
+    pylab.imsave("reconstruction_astra.png",reconstruction_astra[:,:,size/2])
     #Save slice of the bp
-    pylab.imsave("bp.png",bp[:,:,size/2])
+    pylab.imsave("bp_astra.png",bp_astra[:,:,size/2])
     #Save slice of the fp
-    pylab.imsave("fp.png",fp[size/2,:,:])
+    pylab.imsave("fp_astra.png",fp_astra[size/2,:,:])
+
+    #Test fut functions
+    fp_fut, elapsed = test_futhark_fp(phantom=cube, proj_geom=proj_geom, vol_geom=vol_geom)
+    pylab.imsave("fp_fut.png",fp_fut[size/2,:,:])
 
 
 
