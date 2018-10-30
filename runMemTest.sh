c844f3945fda3df28170b97852246c35eaebabc6
Lærke Pedersen <laerke.b.pedersen@gmail.com>
2018-10-30 16:53:20 +0100

    u7pdates

--- runMemTest.sh
+++ runMemTest.sh
@@ -1,6 +1,6 @@
 echo "compiling"
 cd futhark
-futhark-pyopencl forwardprojection.fut
+futhark-opencl forwardprojection.fut
 echo "running benchmarks"
 cat ../test | ./forwardprojection -D -b -t /dev/stderr 1>/dev/null 2>mem_usage_out
 cd ..
