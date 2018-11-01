b9c95ab4a8c820a6c3fa1588bdae31edaca5f147
Lærke Pedersen <laerke.b.pedersen@gmail.com>
2018-11-01 22:34:06 +0100

    forward projection benchmarking

--- runscripts/benchmarkforwardprojection.sh
+++ runscripts/benchmarkforwardprojection.sh
@@ -10,9 +10,9 @@ futhark-opencl forwardprojection_doubleparallel.fut
 futhark-opencl forwardprojection_map.fut
 echo "running benchmarks memory"
 
-./forwardprojection_jh -D < ../data/fpinputf32rad256_32 1> /dev/null 2> ../output/forwardprojection_jh
-./forwardprojection_doubleparallel -D < ../data/fpinputf32rad256_32 1> /dev/null 2>../output/forwardprojection_doubleparallel
-./forwardprojection_map -D < ../data/fpinputf32rad256_32 1> /dev/null 2> ../output/forwardprojection_map
+./forwardprojection_jh -D < ../data/fpinputf32rad256_64 1> /dev/null 2> ../output/forwardprojection_jh
+./forwardprojection_doubleparallel -D < ../data/fpinputf32rad256_64 1> /dev/null 2>../output/forwardprojection_doubleparallel
+./forwardprojection_map -D < ../data/fpinputf32rad256_64 1> /dev/null 2> ../output/forwardprojection_map
 echo "running benchmarks"
 
 futhark-bench --runs=1 --skip-compilation forwardprojection_jh.fut > ../output/forwardprojection_jh_benchmark
