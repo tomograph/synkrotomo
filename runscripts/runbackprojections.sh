44c6e07fe55ce55abc924db7d76e11e38ebaa3be
Lærke Pedersen <laerke.b.pedersen@gmail.com>
2018-11-01 22:37:21 +0100

    merging

--- runscripts/runbackprojections.sh
+++ runscripts/runbackprojections.sh
@@ -2,11 +2,10 @@ echo "runbackprojections"
 echo "compiling"
 cd ..
 cd futhark
-
 futhark-pyopencl --library backprojection_jh.fut
 futhark-pyopencl --library backprojection_doubleparallel.fut
 futhark-pyopencl --library backprojection_map.fut
+# futhark-pyopencl --library backprojection_semiflat.fut
 echo "saving backprojections"
-
 cd ..
 python generatebackprojections.py
