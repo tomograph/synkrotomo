0827278d6b390285092934ded8e3cee81f72c799
Mette Bjerg Lindhøj <memo@di.ku.dk>
2018-11-01 22:33:27 +0100

    updated scripts

--- runscripts/runsinograms.sh
+++ runscripts/runsinograms.sh
@@ -6,6 +6,8 @@ cd futhark
 futhark-pyopencl --library forwardprojection_jh.fut
 futhark-pyopencl --library forwardprojection_doubleparallel.fut
 futhark-pyopencl --library forwardprojection_map.fut
+futhark-pyopencl --library forwardprojection_semiflat.fut
+futhark-pyopencl --library forwardprojection_dpintegrated.fut
 echo "saving sinograms"
 
 cd ..
