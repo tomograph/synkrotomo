c38f8bf8110868a6398406eb754230677a7e9812
Mette Bjerg Lindhøj <memo@di.ku.dk>
2018-11-01 20:51:15 +0100

    fixed runscripts

--- runscripts/benchmarkbackprojection.sh
+++ runscripts/benchmarkbackprojection.sh
@@ -1,3 +1,5 @@
+numruns=1
+echo "benchmarkbackprojection"
 echo "generating data"
 cd ..
 python backprojectiondata.py
@@ -7,12 +9,13 @@ cd futhark
 futhark-opencl backprojection_jh.fut
 futhark-opencl backprojection_doubleparallel.fut
 futhark-opencl backprojection_map.fut
-echo "running benchmarks"
+echo "running benchmarks memory"
 
 ./projectionmatrix_jh -D < ../data/bpinputf32rad256_32 1> /dev/null 2> ../output/backprojection_jh
 ./projectionmatrix_doubleparallel -D < ../data/bpinputf32rad256_32 1> /dev/null 2> ../output/backprojection_doubleparallel
 ./projectionmatrix_map -D < ../data/bpinputf32rad256_32 1> /dev/null 2> ../output/backprojection_map
 
-futhark-bench --runs=10 --skip-compilation backprojection_jh.fut > ../output/backprojection_jh_benchmark
-futhark-bench --runs=10 --skip-compilation backprojection_doubleparallel.fut > ../output/backprojection_doubleparallel_benchmark
-futhark-bench --runs=10 --skip-compilation backprojection_map.fut > ../output/backprojection_map_benchmark
+echo "running benchmarks"
+futhark-bench --runs=$numruns --skip-compilation backprojection_jh.fut > ../output/backprojection_jh_benchmark
+futhark-bench --runs=$numruns --skip-compilation backprojection_doubleparallel.fut > ../output/backprojection_doubleparallel_benchmark
+futhark-bench --runs=$numruns --skip-compilation backprojection_map.fut > ../output/backprojection_map_benchmark
