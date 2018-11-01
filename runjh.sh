07a99b6377e48874dd399f0206bfef01bbec3390
Mette Bjerg Lindhøj <memo@di.ku.dk>
2018-11-01 14:45:39 +0100

    cleanup

--- runjh.sh
+++
@@ -1,8 +0,0 @@
-echo "compiling"
-cd sysmatjh
-futhark-pyopencl --library intersections.fut
-cd ..
-cd sysmatas
-futhark-pyopencl --library algorithm.fut
-cd ..
-python system_matrix_test.py
--- runscripts/benchmarkprojectionmatrix.sh
+++ runscripts/benchmarkprojectionmatrix.sh
@@ -1,4 +1,5 @@
 echo "generating data"
+cd ..
 python projectionmatrixdata.py
 echo "compiling"
 cd futhark
@@ -8,9 +9,9 @@ futhark-opencl projectionmatrix_doubleparallel.fut
 futhark-opencl projectionmatrix_map.fut
 echo "running benchmarks"
 
-./projectionmatrix_jh -D < ../data/matrixinputf32rad256 1> /dev/null 2> ../output/matrix
-./projectionmatrix_doubleparallel -D < ../data/matrixinputf32rad256 1> /dev/null 2>> ../output/matrix
-./projectionmatrix_map -D < ../data/matrixinputf32rad256 1> /dev/null 2>> ../output/matrix
+./projectionmatrix_jh -D < ../data/matrixinputf32rad256 1> /dev/null 2> ../../output/matrix
+./projectionmatrix_doubleparallel -D < ../data/matrixinputf32rad256 1> /dev/null 2>>../../output/matrix
+./projectionmatrix_map -D < ../data/matrixinputf32rad256 1> /dev/null 2>> ../../output/matrix
 
 futhark-bench --runs=10 --skip-compilation projectionmatrix_jh.fut
 futhark-bench --runs=10 --skip-compilation projectionmatrix_doubleparallel.fut
