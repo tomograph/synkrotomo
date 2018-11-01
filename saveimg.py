a91ae7f52118a49653f433f2eb76ff7e75c20244
Lærke Pedersen <laerke.b.pedersen@gmail.com>
2018-11-01 19:32:42 +0100

    might be able to save img piped from stdin

---
+++ saveimg.py
@@ -0,0 +1,30 @@
+from futhark import forwardprojection_doubleparallel
+from futhark import forwardprojection_jh
+from futhark import forwardprojection_map
+import numpy as np
+import tomo_lib
+import sys
+
+def main(argv):
+    # size = 2048
+    # theta_deg = tomo_lib.get_angles(size)
+    # theta_rad = tomo_lib.get_angles(size, degrees=False)
+    # rays = tomo_lib.get_rays(size)
+    # phantom = tomo_lib.get_phantom(size)
+    # tomo_lib.savesinogram("output//sinogram_standard.png",tomo_lib.sinogram(phantom, theta_deg), numrays=len(rays), numangles=len(theta_deg))
+
+    # fp_doubleparallel = forwardprojection_doubleparallel.forwardprojection_doubleparallel()
+    # dp_result = fp_doubleparallel.main( theta_rad.astype(np.float32), rays.astype(np.float32), phantom.flatten().astype(np.float32), size).get()
+    # tomo_lib.savesinogram("output//sinogram_dp.png",dp_result, numrays=len(rays), numangles=len(theta_rad))
+    #
+    # fp_jh = forwardprojection_jh.forwardprojection_jh()
+    # jh_result = fp_jh.main( theta_rad.astype(np.float32), rays.astype(np.float32), phantom.flatten().astype(np.float32), size).get()
+    # tomo_lib.savesinogram("output//sinogram_jh.png",jh_result, numrays=len(rays), numangles=len(theta_rad))
+
+    # fp_map = forwardprojection_map.forwardprojection_map()
+    map_result = argv[1].get()
+    tomo_lib.savesinogram("output//sinogram_map.png",map_result, numrays=, numangles=)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
