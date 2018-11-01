8f36f37054fffea090a0b221b7e2f0dd74a6d67e
Lærke Pedersen <laerke.b.pedersen@gmail.com>
2018-11-01 22:28:17 +0100

    work on benchmarking

--- runscripts/benchmarkforwardprojection.sh
+++ runscripts/benchmarkforwardprojection.sh
@@ -1,7 +1,7 @@
 echo "benchmarkforwardprojection"
 echo "generating data"
 cd ..
-python forwardprojectiondata.py
+# python forwardprojectiondata.py
 echo "compiling"
 cd futhark
 
@@ -10,9 +10,9 @@ futhark-opencl forwardprojection_doubleparallel.fut
 futhark-opencl forwardprojection_map.fut
 echo "running benchmarks memory"
 
-./projectionmatrix_jh -D < ../data/fpinputf32rad256_32 1> /dev/null 2> ../output/forwardprojection_jh
-./projectionmatrix_doubleparallel -D < ../data/fpinputf32rad256_32 1> /dev/null 2>../output/forwardprojection_doubleparallel
-./projectionmatrix_map -D < ../data/fpinputf32rad256_32 1> /dev/null 2> ../output/forwardprojection_map
+./forwardprojection_jh -D < ../data/fpinputf32rad256_32 1> /dev/null 2> ../output/forwardprojection_jh
+./forwardprojection_doubleparallel -D < ../data/fpinputf32rad256_32 1> /dev/null 2>../output/forwardprojection_doubleparallel
+./forwardprojection_map -D < ../data/fpinputf32rad256_32 1> /dev/null 2> ../output/forwardprojection_map
 echo "running benchmarks"
 
 futhark-bench --runs=1 --skip-compilation forwardprojection_jh.fut > ../output/forwardprojection_jh_benchmark
