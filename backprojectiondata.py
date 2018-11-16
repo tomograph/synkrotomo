65e0e89132b6bb627ccb28df044f30b5f70d374c
Mette Bjerg Lindhøj <memo@di.ku.dk>
2018-11-16 13:01:48 +0100

    Cleaned branch to only contain SIRT in progress code

--- backprojectiondata.py
+++ backprojectiondata.py
@@ -6,16 +6,14 @@ import dataprint_lib
 
 def main(argv):
     sizes = [64,128,256,512,1024,2048,4096]
-    stepsizes = [32,64,128,256]
     for i in sizes:
-        for stepsize in stepsizes:
-            filename = "data//bpinputf32rad"+str(i)+"_"+str(stepsize)
+            filename = "data//bpinputf32rad"+str(i)
             f = open(filename,"w+")
             angles = tomo_lib.get_angles(i, False)
             rays = tomo_lib.get_rays(i)
             phantom = tomo_lib.get_phantom(i)
             sino = tomo_lib.sinogram(phantom,tomo_lib.get_angles(i)).flatten()
-            f.write(dataprint_lib.print_f32array(angles)+" "+dataprint_lib.print_f32array(rays)+" "+dataprint_lib.print_f32array(sino)+" "+str(i)+" "+str(stepsize))
+            f.write(dataprint_lib.print_f32array(angles)+" "+dataprint_lib.print_f32array(rays)+" "+dataprint_lib.print_f32array(sino)+" "+str(i))
 
 if __name__ == '__main__':
     main(sys.argv)
--- forwardprojectiondata.py
+++ forwardprojectiondata.py
@@ -6,15 +6,13 @@ import dataprint_lib
 
 def main(argv):
     sizes = [64,128,256,512,1024,2048,4096]
-    stepsizes = [32,64,128,256]
     for i in sizes:
-        for stepsize in stepsizes:
-            filename = "data//fpinputf32rad"+str(i)+"_"+str(stepsize)
+            filename = "data//fpinputf32rad"+str(i)
             f = open(filename,"w+")
             angles = tomo_lib.get_angles(i, False)
             rays = tomo_lib.get_rays(i)
             phantom = tomo_lib.get_phantom(i).flatten()
-            f.write(dataprint_lib.print_f32array(angles)+" "+dataprint_lib.print_f32array(rays)+" "+dataprint_lib.print_f32array(phantom)+" "+str(stepsize))
+            f.write(dataprint_lib.print_f32array(angles)+" "+dataprint_lib.print_f32array(rays)+" "+dataprint_lib.print_f32array(phantom))
 
 if __name__ == '__main__':
     main(sys.argv)
