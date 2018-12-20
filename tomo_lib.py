473167a6ca1e3e504d210c2d13b9929e8d752701
Mette Bjerg Lindhøj <memo@di.ku.dk>
2018-12-20 21:18:24 +0100

    More debugging of bp

--- tomo_lib.py
+++ tomo_lib.py
@@ -5,32 +5,24 @@ import numpy as np
 import scipy.io
 import scipy.misc
 
-def get_angles(size, degrees=True):
+def get_angles(size):
     num_angles = math.ceil(size*math.pi/2)
-    if degrees:
-        return np.linspace(0, 180, num_angles, False)
-    else:
-        return np.linspace(0, np.pi, num_angles,False)
-
-def sinogram(image, theta):
-    sinogram = np.zeros((len(theta), max(image.shape)))
-    for i in range(0, len(theta)):
-        rotated_image = rotate(image, theta[i], resize=False)
-        sinogram[i] = sum(rotated_image)
-    return sinogram
+    return np.linspace(0, np.pi, num_angles,False)
 
 def get_rays(size):
-    #numrays = np.sqrt(2*(size**2))
-    startvalue = (size-1)/2.0
-    return np.linspace((-(1.0)*startvalue), startvalue, size).astype(np.float32)
+    numrays = np.sqrt(2*(size**2))
+    startvalue = (numrays-1)/2.0
+    return np.linspace((-(1.0)*startvalue), startvalue, numrays).astype(np.float32)
 
 def savesinogram(filename, data, numrays, numangles):
     reshaped = data.reshape((numangles,numrays))
     scipy.misc.toimage(reshaped).save(filename)
 
-def savebackprojection(filename, data, size):
-    max = np.amax(data)
-    scipy.misc.toimage(data.reshape((size,size))).save(filename)
+def savebackprojection(filename, data, size, flip = False):
+    reshaped = data.reshape((size,size))
+    if flip :
+        reshaped = np.flip(reshaped,1)
+    scipy.misc.toimage(reshaped).save(filename)
 
 def get_phantom(size):
     return random_shapes((size, size), min_shapes=5, max_shapes=10, multichannel=False, random_seed=0)[0]/255
