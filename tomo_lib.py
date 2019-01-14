b3f1f9cb3b648a7130024f529bad4009f597e9fd
Mette Bjerg Lindhøj <memo@di.ku.dk>
2019-01-14 16:34:55 +0100

    Made test for SIRT 3D - now make it run and debug

--- tomo_lib.py
+++ tomo_lib.py
@@ -36,6 +36,33 @@ def get_sinogram(phantom, rays, angles):
 
     return result
 
+def get_sinogram3D(phantom, rays, angles):
+    vol_geom = astra.create_vol_geom(phantom.shape)
+    proj_geom =astra.create_proj_geom('parallel3d', 1.0, 1.0, len(rays), len(rays), angles)
+    proj_id, proj_data = astra.create_sino3d_gpu(phantom, proj_geom, vol_geom)
+
+    # Create a data object for the reconstruction
+    volume_id = astra.data3d.create('-vol', vol_geom, phantom)
+
+    # Set up the parameters for a reconstruction algorithm using the GPU
+    cfg = astra.astra_dict("FP_CUDA")
+    cfg['VolumeDataId'] = volume_id
+    cfg['ProjectionDataId'] = proj_id
+    # Create the algorithm object from the configuration structure
+    alg_id = astra.algorithm.create(cfg)
+    astra.algorithm.run(alg_id)
+
+    # Get the result
+    result = astra.data3d.get(proj_id)
+
+    # Clean up. Note that GPU memory is tied up in the algorithm object,
+    # and main RAM in the data objects.
+    astra.algorithm.delete(alg_id)
+    astra.data3d.delete(volume_id)
+    astra.data3d.delete(proj_id)
+
+    return result
+
 def get_angles(size, degrees = False):
     num_angles = math.ceil(size*math.pi/2)
     if degrees:
@@ -65,3 +92,10 @@ def get_phantom(size):
     r = size/2;
     y,x = np.ogrid[-r: r, -r: r]
     return np.where(x**2+y**2 <= r**2, full_image,0.0);
+
+def get_phantom3D(size):
+    #hollow cube
+    cube = np.zeros((size,size,size))
+    cube[size/8:size-size/8,size/8:size-size/8,size/8:size-size/8] = 1
+    cube[size/4:size-size/4,size/4:size-size/4,size/4:size-size/4] = 0
+    return cube;
