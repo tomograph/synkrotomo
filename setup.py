848e9abb12e13cb7804e0ee3d56087f8f84a54e3
Mette Bjerg Lindhøj <memo@di.ku.dk>
2019-01-31 09:56:04 +0100

    Fixed minor issues with samples not running and up+dates in futhark causing problems in make

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(name='synkrotomo',
         description='Synkrotron Computed Tomography',
-        author='Mette Bjerg and Lærke Pedersen',
+        author='Mette Bjerg and Laerke Pedersen',
         author_email='memo@di.ku.dk',
         version='1.0',
         url='https://github.com/tomograph/beatingastra',
