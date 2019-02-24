971334ae579d89d6511d66bc934b4b210e188334
Unknown <memo@di.ku.dk>
2019-02-24 14:35:04 +0100

    Deleted tests
    Former-commit-id: 55a00f6f6451c4d0ec89789fe8ba78c8f82fd973

--- output/20190224_083804/formatfuthark.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd $2
-while read line;\
-     do echo $line;\
-     if [[ $line == *"dataset"* ]];\
-          then numbers=$(echo $line | grep -Eo '[0-9]+([.][0-9]+)?' | tr '\n' ' ' | sed 's/^[ \t]*//;s/[ \t]*$//' | cut -d ' ' -f 2,3 | tr ' ' ';');\
-          echo $numbers >> $1;\
-     fi;\
-done
